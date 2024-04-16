fcc51de093d6a9c0493084e6f7e4aff2e219ede0
teneppa <laurilateemu8@gmail.com>
2024-04-16 09:05:57 +0300

    Add loadout info, change font

---
+++ docs/assets/js/weapon_loadouts.js
@@ -0,0 +1,59 @@
+const weapon_loadouts_str = `[
+    {
+        "weapon": "M16A4",
+        "loadouts": [
+            {
+                "attachments": ["Compensator", "Vertical Foregrip", "Extended QuickDraw Mag", "4x ACOG Scope"]
+            }
+        ]
+    },
+    {
+        "weapon": "M416",
+        "loadouts": [
+            {
+                "attachments": ["Compensator", "Thumb Grip", "Extended QuickDraw Mag"]
+            },
+            {
+                "attachments": ["Compensator", "Half Grip", "Extended QuickDraw Mag"]
+            }
+        ]
+    },
+    {
+        "weapon": "SCAR-L",
+        "loadouts": [
+            {
+                "attachments": ["Compensator", "Thumb Grip", "Extended QuickDraw Mag"]
+            },
+            {
+                "attachments": []
+            }
+        ]
+    },
+    {
+        "weapon": "AUG A3",
+        "loadouts": [
+            {
+                "attachments": ["Compensator", "vertical foregrip", "Extended QuickDraw Mag"]
+            },
+            {
+                "attachments": ["Suppressor", "vertical foregrip", "Extended QuickDraw Mag"]
+            }
+        ]
+    },
+    {
+        "weapon": "AKM",
+        "loadouts": [
+            {
+                "attachments": ["Compensator", "vertical foregrip", "Extended QuickDraw Mag"]
+            }
+        ]
+    },
+    {
+        "weapon": "SKS",
+        "loadouts": [
+            {
+                "attachments": ["Compensator", "Light Grip", "Extended QuickDraw Mag"]
+            }
+        ]
+    }
+]`;
